84e5e0a05ddb9bde11e515fbe2c0403192aecca9
branaphy <61725592+branaphy@users.noreply.github.com>
2023-10-20 00:10:30 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+# The starting point of execution for the Talking Clock. This contains the code pertaining the GUI.
+
+# Authors: Alice Vanni, Amber Lankheet, Brandi Hongell, Jingxuan Yue, Wenjun Meng
