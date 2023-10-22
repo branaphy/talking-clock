5620af3fcb2900a8e7f3c74e9bf5ca07beae87a8
branaphy <61725592+branaphy@users.noreply.github.com>
2023-10-22 20:49:55 +0200

    Create helpers.py

---
+++ helpers.py
@@ -0,0 +1,5 @@
+# This code contains functions used by all languages.
+
+# Also contains necessary code for certain audio functions.
+
+# Authors: Alice Vanni, Amber Lankheet, Brandi Hongell, Jingxuan Yue, Wenjun Meng
