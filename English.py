337932b145c08e6e31330cdd608dde146e8903d0
branaphy <61725592+branaphy@users.noreply.github.com>
2023-10-23 02:07:37 +0200

    Update English.py
    Added logic to speak time in English (hours, minutes)

--- English.py
+++ English.py
@@ -1,3 +1,15 @@
 # This code contains logic related to the linguistic rules of telling time in American English.
 
 # Authors: Alice Vanni, Amber Lankheet, Brandi Hongell, Jingxuan Yue, Wenjun Meng
+
+from helpers import get_current_time, play_audio
+
+
+def speak_time_in_english():
+    hour, minute = get_current_time()
+
+    hour_audio_path = f"EnglishAudio/hour_{int(hour)}.mp3"
+    minute_audio_path = f"EnglishAudio/minute_{minute}.mp3"
+
+    play_audio(hour_audio_path)
+    play_audio(minute_audio_path)
