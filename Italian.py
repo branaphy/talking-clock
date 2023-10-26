a184697462f3fe81d342eb0d0094d6403824558e
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2023-10-26 13:22:53 +0200

    Update Italian.py fixed telling o'clock

--- Italian.py
+++ Italian.py
@@ -20,44 +20,56 @@ def speak_time_in_italian():
     '''
     hour, minute, ampm = get_current_time()
     try:
-    #First, select the prosody for the minutes
+        # First, select the prosody for the minutes
         minute_prosody = "falling" if (0 <= int(minute) <= 37 
                                        or 58 <= int(minute) <= 59) else "rising"
-    #Select the closest multiple of 5 for the minutes
+        # Select the closest multiple of 5 for the minutes
         minute = round(int(minute) / 5) * 5
         minute = "0" + str(minute) if len(str(minute)) == 1 else minute
-    #Select the audio file for minutes
-        minute_audio = f"ItalianAudio/{minute}_{minute_prosody}_it.wav"\
-            if int(minute) < 35 else f"ItalianAudio/{(60-int(minute))}_{minute_prosody}_it.wav"
+        # Select the audio file for minutes
+        if int(minute) > 35:
+            if len(str(60-int(minute))) == 1:
+                minute = "0" + str(60-int(minute))
+                minute_audio = f"ItalianAudio/{minute}_{minute_prosody}_it.wav"
+            else:
+                minute = (60-int(minute))
+        minute_audio = f"ItalianAudio/{minute}_{minute_prosody}_it.wav"
     
-    #Select then the prosody for the hour
+        # Select then the prosody for the hour
         hour_prosody = "falling" if minute_prosody == "rising" else "rising"
-        hour = int(hour) + 1 if hour_prosody == "falling" else hour
-    #To select the hour, we simply select the current hour, except for
-    #midnight and midday, in which we take into account if the time is am or pm.
+        if hour_prosody == "falling":
+            hour = int(hour) + 1
+            if int(hour) == 13:
+                hour = "01"
+        hour = "0" + str(hour) if len(str(hour)) == 1 else hour
+        # To select the hour, we simply select the current hour, except for
+        # midnight and midday, in which we take into account if the time is am or pm.
         hour_audio = f"ItalianAudio/12{ampm.lower()}_{hour_prosody}_it.wav"\
             if int(hour) == 12 else f"ItalianAudio/{hour}_{hour_prosody}_it.wav"
     
-    #Select the linking word according to minutes and hour
-    #The sequence in which hour and minute will be told is also selected
-    #here based on minute prosody
+        # Select the linking word according to minutes and hour
+        # The sequence in which hour and minute will be told is also selected
+        # here based on minute prosody
         if minute == "00":
-            audio_sequence = (hour_audio.replace("rising", "falling"))
+            audio_sequence = hour_audio.replace("rising", "falling")
         elif minute_prosody == "rising":
             linking_audio = "ItalianAudio/a.wav" if int(hour) == 12\
                 else "ItalianAudio/alle.wav"
             audio_sequence = (minute_audio, linking_audio, hour_audio)
         else:
             linking_audio = "ItalianAudio/and.wav"
-            audio_sequence = (hour_audio, linking_audio, minute_audio)
+            audio_sequence = (hour_audio, linking_audio, minute_audio)   
+            
+        if type(audio_sequence) == str:
+            play_audio(audio_sequence)
+        else:
+            for audio in audio_sequence:
+                play_audio(audio)
     
-        for audio in audio_sequence:
-            play_audio(audio)
-    
-    except Exception:
+    except Exception as e:
         # Create a tts object in Italian to speak the error
-        #It will say "I'm sorry, an error occured" in Italian
-        ita_tts = gTTS(text="Mi dispiace, si è verificato un errore", lang='it')
+        # It will say "I'm sorry, an error occured" in Italian
+        ita_tts = gTTS(text="Mi dispiace, si è verificato un errore", lang="it", slow=False)
         # Save the temporary file to play it with pygame
         temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".mp3")
         file_path = temp_file.name
