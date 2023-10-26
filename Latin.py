b39361cd4f98e33ab922f9c68deba345041c1456
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2023-10-26 11:17:44 +0200

    Update Latin.py

--- Latin.py
+++ Latin.py
@@ -2,14 +2,15 @@
 
 # Authors: Alice Vanni, Amber Lankheet, Brandi Hongell, Jingxuan Yue, Wenjun Meng
 
-from helpers import get_current_time, play_audio
 from gtts import gTTS
 import tempfile
+from helpers import get_current_time, play_audio
 
 def speak_time_in_latin():
     '''
     The speak_time_in_latin() function takes no arguments.
-    It generates a string that express the current time according to the local time.
+    It generates a string that express the current time according 
+    to the local time.
 
     Returns
     -------
@@ -21,15 +22,22 @@ def speak_time_in_latin():
     hour, minute, ampm = get_current_time()
     
     # Define Latin numbers to express hours
-    latin_numbers = ["unus", "duo", "tres", "quattuor", "quinque", "sex", "septem", "octo", "novem", "decem", "undecim", "duodecim",
-                      "tredecim","quattuordecim", "quindecim", "sedecim", "septendecim", "octodecim", "novemdecim", 
-                      "viginti", "viginti unus", "viginti duo", "viginti tres", "viginti quattuor", "viginti quinque", "viginti sex", 
-                      "viginti septem", "viginti octo", "viginti novem", "triginta", "triginta unus", "triginta duo", "triginta tres",
-                      "triginta quattuor", "triginta quinque", "triginta sex", "triginta septem", "triginta octo", "triginta novem", 
-                      "quadraginta", "quadraginta unus", "quadraginta duo", "quadraginta tres", "quadraginta quattuor", "quadraginta quinque",
-                      "quadraginta sex", "quadraginta septem", "quadraginta octo", "quadraginta novem", "quinquaginta", "quinquaginta unus", 
-                      "quinquaginta duo", "quinquaginta tres", "quinquaginta quattuor", "quinquaginta quinque", "quinquaginta sex", 
-                      "quinquaginta septem", "quinquaginta octo", "quinquaginta novem"]
+    latin_numbers = ["unus", "duo", "tres", "quattuor", "quinque", "sex", 
+                     "septem", "octo", "novem", "decem", "undecim", "duodecim",
+                     "tredecim","quattuordecim", "quindecim", "sedecim",
+                     "septendecim", "octodecim", "novemdecim", "viginti", 
+                     "viginti unus", "viginti duo", "viginti tres", 
+                     "viginti quattuor", "viginti quinque", "viginti sex", 
+                     "viginti septem", "viginti octo", "viginti novem", "triginta",
+                     "triginta unus", "triginta duo", "triginta tres", "triginta quattuor",
+                     "triginta quinque", "triginta sex", "triginta septem", "triginta octo", 
+                     "triginta novem", "quadraginta", "quadraginta unus", "quadraginta duo",
+                     "quadraginta tres", "quadraginta quattuor", "quadraginta quinque",
+                      "quadraginta sex", "quadraginta septem", "quadraginta octo", 
+                     "quadraginta novem", "quinquaginta", "quinquaginta unus", 
+                      "quinquaginta duo", "quinquaginta tres", "quinquaginta quattuor", 
+                     "quinquaginta quinque", "quinquaginta sex", "quinquaginta septem", 
+                     "quinquaginta octo", "quinquaginta novem"]
     # Convert the current time to Latin and select the current time
     latin_hour = latin_numbers[int(hour)-1]
     if minute == 0:
