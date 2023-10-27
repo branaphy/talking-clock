6ad2a38a36318210287dea088cea66c4a4696ca1
AliceVanni <144113867+AliceVanni@users.noreply.github.com>
2023-10-27 14:01:50 +0200

    Update Italian.py

--- Italian.py
+++ Italian.py
@@ -18,8 +18,8 @@ def speak_time_in_italian():
     This function does not return a value; it plays audio to speak the time.
 
     '''
-    #hour, minute, ampm = get_current_time()
-    hour, minute, ampm = ['12', '24', 'pm']
+    hour, minute, ampm = get_current_time()
+    #hour, minute, ampm = ['12', '24', 'pm']
     try:
         # Select the closest multiple of 5 for the minutes
         minute = format(round(int(minute) / 5) * 5, '02')
